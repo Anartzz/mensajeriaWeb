a8e2d1e25aa21443cde14af51b85fc6e8e8653a8
Anartz <anartz.pagaldai@gmail.com>
2023-06-21 20:53:28 +0200

    a

--- src/vista/agregar/agregar.js
+++ src/vista/agregar/agregar.js
@@ -1,4 +1,17 @@
 const usuarios = document.querySelector("#usuarios")
-
-usuarios.innerHTML += `<option value="1">1</option>`
+const nombreUsuario = document.querySelector("#nombreUsuario")
+let lenghtAnterior = 0
+nombreUsuario.addEventListener("input", () => {
+    if (nombreUsuario.value.lenght == 1 && lenghtAnterior == 0) {
+        fetch(`${window.location.protocol}//${window.location.hostname}:${window.location.port}/agregar`, {
+            method: "post",
+            headers: {
+                'Content-Type': 'text/plain'
+            },
+            body: nombreUsuario.value
+        })
+    }
+    lenghtAnterior = nombreUsuario.value.lenght
+    
+})
 // hacer request dapara le primera letra luego filtrar con js
